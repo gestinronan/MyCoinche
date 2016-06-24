a0111c4f6c7a490e0669b6cc593967c208071955
GESTIN Ronan <ronan.gestin@orange.com>
2016-06-24 15:44:58 +0200

    Change version + AA

---
+++ coincheCounter/app/src/main/java/com/rgestin/coinchecounter/MainActivity.java
@@ -0,0 +1,12 @@
+package com.rgestin.coinchecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+
+}
